e88807557dd4fcd47cbc0bbe72bee182c20569d6
Ryan Bregier <rabunc@gmail.com>
2017-09-18 21:05:42 -0400

    setup handlebars

--- models/habit.js
+++ models/habit.js
@@ -6,6 +6,10 @@ module.exports = function (sequelize, DataTypes) {
             validate: {
                 len: [1]
             }
+        },
+        make: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false
         }
     });
     Habit.associate = (function (models) {
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,11 +1,23 @@
 var path = require("path");
+var db = require("../models")
 
 // Routes
 // =============================================================
-module.exports = function(app) {
-// index route loads view.html
-  app.get("/", function(req, res) {
+module.exports = function (app) {
+  // index route loads view.html
+  app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/overview.html"));
   });
-
+  app.get("/user/:id", function (req, res) {
+    db.User.findOne({
+      include: [{
+        model: db.Habit,
+        where: {
+          id: req.params.id
+        }
+      }]
+    }).then(function(result){
+      res.json(result)
+    });
+  });
 }
