39fdd31748d6700523273621a87e4b68462c7e8b
Terri Pannell <terripannell@gmail.com>
2017-09-20 19:31:28 -0400

    modified routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,1 +1,10 @@
 var path = require("path");
+
+// Routes
+// =============================================================
+module.exports = function (app) {
+    // index route loads view.html
+  app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/overview.html"));
+  });
+}
