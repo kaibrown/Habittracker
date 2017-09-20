e5c90ca325abb45ce8f37d90649739f81fc1f32e
Ryan Bregier <rabunc@gmail.com>
2017-09-20 19:35:32 -0400

    server.js route updated

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.set("view engine", "handlebars");
 // Routes
 // =============================================================
 require("./routes/html-routes.js")(app);
-// require("./routes/author-api-routes.js")(app);
+require("./routes/api-routes.js")(app);
 // require("./routes/post-api-routes.js")(app);
 
 // Syncing our sequelize models and then starting our Express app
