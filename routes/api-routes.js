51f8896cc5a1e1966adcf0998a7aa0020e707b43
Ryan Bregier <rabunc@gmail.com>
2017-09-20 22:02:20 -0400

    create habit working

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -6,44 +6,52 @@ var db = require("../models")
 module.exports = function (app) {
 
 
-  app.get("/habit/:id", function(req, res){
-    var query = {};
-    if (req.query.author_id) {
-      query.UsesrId = req.query.user_id;
-    }
-    db.Habit.findOne({
-      where: query,
-      include: [db.Progress]
-    }).then(function(result) {
-      res.json(result);
+    app.get("/habit/:id", function (req, res) {
+        var query = {};
+        if (req.query.author_id) {
+            query.UsesrId = req.query.user_id;
+        }
+        db.Habit.findOne({
+            where: query,
+            include: [db.Progress]
+        }).then(function (result) {
+            res.json(result);
+        });
     });
-  });
-  
-  app.get("/user/:id", function (req, res) {
-    // db.User.findAll({}).then(function(result){
-    //   res.json(result);
-    // });
-    db.User.findOne({
-      where: {
-        id: req.params.id
-      },
-      include: [db.Habit]
-    }).then(function(result){
-      //console.log(result.Habits[0]);
-      //console.log(result.Habits[1]);
 
-      var hbsObject = {
-        habit:result.Habits[0]
-      }
+    app.post("/api/createhabit", function (req, res) {
+        console.log(req.body)
+        db.Habit.create(req.body).then(function (result) {
+            res.json(result)
+            console.log(result)
+        })
+    })
 
-      var array = [];
-      for(var i=0;i<result.Habits.length;i++){
-        array.push(result.Habits[i]);
-      }
-      
-      res.render("index", {habits : array})
+    app.get("/user/:id", function (req, res) {
+        // db.User.findAll({}).then(function(result){
+        //   res.json(result);
+        // });
+        db.User.findOne({
+            where: {
+                id: req.params.id
+            },
+            include: [db.Habit]
+        }).then(function (result) {
+            //console.log(result.Habits[0]);
+            //console.log(result.Habits[1]);
+
+            var hbsObject = {
+                habit: result.Habits[0]
+            }
+
+            var array = [];
+            for (var i = 0; i < result.Habits.length; i++) {
+                array.push(result.Habits[i]);
+            }
+
+            res.render("index", { habits: array })
+        });
     });
-  });
 }
 
 
