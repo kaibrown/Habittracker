bd46305e789f2e6db9d376a76fa695cd83f91073
Terri Pannell <terripannell@gmail.com>
2017-09-27 15:19:14 -0400

    added creating user to test case

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@
 
 var Nightmare = require("nightmare");
 var expect = require("chai").expect;
-var xpath = require("xpath")
+var xpath = require("xpath");
 
 describe("Habittracker", function () {
   // The default tests in mocha is 2 seconds.
@@ -10,25 +10,50 @@ describe("Habittracker", function () {
   var login = ".user-login-link";
   var myHabits = "#my-habits-link";
   var goodHabitBox = "#entergoodHabit";
+  var badHabitBox = "#enterbadHabit";
   var submitMake = "#makeSubmit";
-  var backLink = "#back-link"
+  var submitBreak = "#breakSubmit";
+  var backLink = "#back-link";
+  var createUser = "#makehabit";
+  var usr = "#usr";
+  var pwd = "#pwd";
+  var create = "#createButton";
 
-  this.timeout(10000);
-  it("should create a new habit", function (done) {
+  this.timeout(30000);
+  it("should create a new habit after creating test user", function (done) {
     // ID for the login button.
     Nightmare({ show: true })
       .goto("http://localhost:8080")
+      //Just to be safe;
+      .wait(createUser)
+      .screenshot("landingPage.png")
+      //Click the create user button
+      .click(createUser)
+      //Steps to create user
+      .wait(pwd)
+      .click(pwd)
+      .type(pwd, "password")
+      .wait(usr)
+      .click(usr)
+      .type(usr, "test1")
+
+      .wait(create)
+      .click(create)
       // Just to be safe.
       .wait(login)
       // Click the login button.
       .click(login)
-      // Evaluate the title
+      // Evaluate the making new habits
       .wait(myHabits)
       .click(myHabits)
       .wait(goodHabitBox)
       .click(goodHabitBox)
-      .type(goodHabitBox, "Test Habit")
+      .type(goodHabitBox, "Make1")
       .click(submitMake)
+      .wait(badHabitBox)
+      .click(badHabitBox)
+      .type(badHabitBox, "Break1")
+      .click(submitBreak)
       .wait(backLink)
       .click(backLink)
       .wait(login)
